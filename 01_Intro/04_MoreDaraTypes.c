8e331b97baa132c4658d286557f77ed9222427cd
Luke <lrsorrenti@hotmail.com>
2024-02-27 20:43:44 -0600

    Added long long signed and unsigned data type examples

--- 01_Intro/04_MoreDaraTypes.c
+++ 01_Intro/04_MoreDaraTypes.c
@@ -22,13 +22,27 @@ int main() {
     // doubles, doubles use %lf to string template
     double faveNum = 12.1212121212121212;
     // use 0.totalDigits in the print statement to show 
-    // that num
+    // that num, also a double uses 8 bytes of memory, where 
+    // a regular float uses 4 bytes
     printf("\n %0.15lf", faveNum);
     int age = 23;
     printf("\n %d", age);
 
-    
-    
-    
+    // Booleans in C, to use booleans you'll need to 
+    // import the standard boolean library <stdbool.h>
+
+    bool notCool = false;
+    bool cool = true;
+    printf("\n not cool is false and false in c is represented by a %d", notCool);
+    printf("\n cool is true and true in c is represented by a %d", cool);
+
+    // To specify very long integers use long long 
+    // specifier, these long longs use 8 bytes of memory
+    long long int myNum = 123456789101112131;
+    // for unsigned use %lld
+    printf("\n Long long %lld \n", myNum);
+    // for signed long long use %llu
+    unsigned long long int newInt = 11234567891011121316U;
+    printf("\n signed long long int %llu \n", newInt);
     return 0;
 }
